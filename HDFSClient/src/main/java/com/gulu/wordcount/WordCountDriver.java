c5605262dd72aa08bdcf532ffe4864d3e52a7ed6
sherlock <1105026730@qq.com>
2025-02-11 17:30:53 +0800

    job driver

--- HDFSClient/src/main/java/com/gulu/wordcount/WordCountDriver.java
+++ HDFSClient/src/main/java/com/gulu/wordcount/WordCountDriver.java
@@ -1,8 +1,40 @@
 package com.gulu.wordcount;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 /**
  * @author Gollum
  * @date 2024-12-17 22:38
  */
 public class WordCountDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        // 获取配置信息及job对象
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration);
+
+        job.setJarByClass(WordCountDriver.class);
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setReducerClass(WordCountReduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean b = job.waitForCompletion(true);
+        System.exit(b ? 0 : 1);
+
+    }
 }
